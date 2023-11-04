1011cd748b290646a782b9ce0b62a2b43eb6b062
LoGgI <lasse_aakjaer@hotmail.com>
2023-11-04 20:28:11 +0100

    updated cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -7,15 +7,19 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(request: Request) {
-  // if (
-  //   request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  // ) {
-  //   return NextResponse.json({
-  //     status: 401,
-  //     body: "Unauthorized",
-  //   })
-  // }
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const secret = url.searchParams.get("cron_api_secret")
+
+  if (
+    secret !== process.env.CRON_SECRET
+    // || request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({
+      status: 401,
+      body: "Unauthorized",
+    })
+  }
 
   try {
     await resend.emails.send({
