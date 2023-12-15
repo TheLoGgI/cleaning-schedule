961afa4a03fd9f01246e7deaf1070a2e9e2e3072
LoGgI <lasse_aakjaer@hotmail.com>
2023-12-15 21:04:38 +0100

    fix invite button

--- src/app/server/quries/genereateInviteCode.ts
+++ src/app/server/quries/genereateInviteCode.ts
@@ -6,14 +6,30 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 export const generateInviteCode = async (scheduleId: string) => {
   const generatedCode = Math.random().toString(36).substring(2, 15)
 
-  const supabase = createServerComponentClient({ cookies })
-
-  const inviteKey = await supabase.from("InviteKey").upsert(
-    { scheduleId, key: generatedCode },
+  const supabase = createServerComponentClient(
+    { cookies },
     {
-      onConflict: "scheduleId",
+      // supabaseKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+      options: {
+        global: {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json, */*; ",
+          },
+        },
+      },
     }
   )
+
+  const inviteKey = await supabase
+    .from("InviteKey")
+    .upsert(
+      { scheduleId, key: generatedCode }
+      // {
+      //   onConflict: "scheduleId",
+      // }
+    )
+    .explain()
 
   if (inviteKey.error) {
     const existingInviteKey = await supabase
