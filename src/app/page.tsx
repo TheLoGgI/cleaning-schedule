d09c85d7f9bf132b68d7d0da6baf103b1da56b4c
LoGgI <lasse_aakjaer@hotmail.com>
2023-12-28 18:56:09 +0100

    improve: added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import FeatureSection from "./components/landing/FeatureSection"
 import HeroSection from "./components/landing/HeroSection"
+import { Metadata } from "next"
 import PricingSection from "./components/landing/PricingSection"
 
 export const dynamic = "force-dynamic"
+
+export const metadata: Metadata = {
+  title: "Manage Your Cleaning Schedule with Ease",
+  description: "Simplify Your Cleaning Routine, and Get More Done",
+}
 
 export default async function Home() {
   return (
