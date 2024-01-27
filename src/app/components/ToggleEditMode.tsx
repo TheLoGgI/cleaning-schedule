5f826b48cff2f3a04c6559f4342fe7d8e340641d
TheLoGgI <lasse_aakjaer@hotmail.com>
2024-01-27 23:16:01 +0100

    improve swap button

--- src/app/components/ToggleEditMode.tsx
+++ src/app/components/ToggleEditMode.tsx
@@ -1,17 +1,18 @@
 "use client"
 
+import { twMerge } from "tailwind-merge"
 import { useStore } from "../hooks/useStore"
 
 
 export const ToggleEditModeButton = () => {
 
-  const {toggleEditMode} = useStore()
+  const {editMode, toggleEditMode} = useStore()
  
   return <button
-  className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  className={twMerge("inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800")}
   type="button"
   onClick={toggleEditMode}
 >
-  Toggle Swap mode
+  {editMode ? "Toggle OFF Swap" : "Toggle ON Swap" }
 </button>
 }
