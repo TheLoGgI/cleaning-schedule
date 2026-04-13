a58ce19fc48207e2408a31c0a33014039025120a
TheLoGgI <lasse_aakjaer@hotmail.com>
2026-04-13 20:57:20 +0200

    move cron

---
+++ src/app/api/cron/mail-cron-job.ts
@@ -0,0 +1,109 @@
+"use server"
+
+// Add to vercel.json to activate Cron job
+// {
+//       "path": "/api/cron?cron_api_secret=D1pkAYpS5HRNyUKGQSMC1epxYsd44U&debug=true",
+//       "schedule": "0 16 * * 6"
+//     },
+
+import { NextResponse } from "next/server"
+import { NotifyUsersOfSchedule } from "@/app/components/emails/notifyUserOfSchedule"
+import { Resend } from "resend"
+import { cookies } from "next/headers"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { getWeekNumber } from "@/app/helpers/getWeekNumber"
+import { rollSchedules } from "@/app/server/actions/rollSchedulesJob"
+
+const resend = new Resend(process.env.RESEND_API_KEY ?? undefined)
+
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const secret = url.searchParams.get("cron_api_secret")
+  const debugMode = url.searchParams.get("debug")
+
+  // TODO: Add Authorization header check
+  if (
+    secret !== process.env.CRON_SECRET
+    // || request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({
+      status: 401,
+      body: "Unauthorized",
+    })
+  }
+
+  // Get current week number
+  const today = new Date()
+  const currentWeekNumber = getWeekNumber(today)
+
+  const supabase = createServerComponentClient({ cookies })
+  const nextWeekScheduleRows = await supabase
+    .from("ScheduleRow")
+    .select(
+      `
+    scheduleId,
+    room: Room(id, activeInSchedule, roomNr, User(id, firstName, lastName, email, notification))
+    `
+    )
+    .eq("weekNr", currentWeekNumber + 1)
+
+  const emailRecipients = []
+  for (const scheduleRow of nextWeekScheduleRows.data ?? []) {
+    const scheduleId = scheduleRow.scheduleId
+    delete scheduleRow.scheduleId // Remove scheduleId from object, destructing behavior
+    const usersOfScheduleRow = Object.values(scheduleRow).filter(
+      (user) => user.User.email && user.User.notification
+    )
+
+    emailRecipients.push(
+      ...usersOfScheduleRow.map((user) => ({ scheduleId, ...user }))
+    )
+  }
+
+  
+if (debugMode == "true") {
+  const names = emailRecipients.map((recipient) => recipient.User.firstName)
+  await resend.emails.send({
+    from: "ME <business@lasseaakjaer.com>",
+    to: "lasse_aakjaer@hotmail.com",
+    subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
+    text: JSON.stringify(names) + " was sent email of schedule week",
+  })
+  
+  await rollSchedules()
+
+  return NextResponse.json({ query: emailRecipients }, { status: 200 })
+} 
+
+  for (const recipient of emailRecipients) {
+    const responseResend = await resend.emails.send({
+      from: "ME <business@lasseaakjaer.com>",
+      to: [recipient.User.email],
+      subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
+      react: NotifyUsersOfSchedule({
+        username: `${recipient.User.firstName} ${recipient.User.lastName}`,
+        scheduleId: recipient.scheduleId,
+      }),
+    })
+
+    console.info(
+      "Email sent to ",
+      recipient.User.email,
+      " for roomId: ",
+      responseResend.data?.id
+    )
+  }
+  
+  await resend.emails.send({
+    from: "ME <business@lasseaakjaer.com>",
+    to: "lasse_aakjaer@hotmail.com",
+    subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
+    text: JSON.stringify(emailRecipients) + " was sent email of schedule week",
+  })
+  
+
+
+  
+  return NextResponse.json({ query: emailRecipients }, { status: 200 })
+}
+
--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,20 +1,7 @@
-"use server"
-
-// Add to vercel.json to activate Cron job
-// {
-//       "path": "/api/cron?cron_api_secret=D1pkAYpS5HRNyUKGQSMC1epxYsd44U&debug=true",
-//       "schedule": "0 16 * * 6"
-//     },
-
 import { NextResponse } from "next/server"
-import { NotifyUsersOfSchedule } from "@/app/components/emails/notifyUserOfSchedule"
-import { Resend } from "resend"
-import { cookies } from "next/headers"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { getWeekNumber } from "@/app/helpers/getWeekNumber"
-import { rollSchedules } from "@/app/server/actions/rollSchedulesJob"
 
-const resend = new Resend(process.env.RESEND_API_KEY ?? undefined)
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
 
 export async function GET(request: Request) {
   const url = new URL(request.url)
@@ -30,80 +17,11 @@ export async function GET(request: Request) {
       status: 401,
       body: "Unauthorized",
     })
-  }
-
-  // Get current week number
-  const today = new Date()
-  const currentWeekNumber = getWeekNumber(today)
-
-  const supabase = createServerComponentClient({ cookies })
-  const nextWeekScheduleRows = await supabase
-    .from("ScheduleRow")
-    .select(
-      `
-    scheduleId,
-    room: Room(id, activeInSchedule, roomNr, User(id, firstName, lastName, email, notification))
-    `
-    )
-    .eq("weekNr", currentWeekNumber + 1)
-
-  const emailRecipients = []
-  for (const scheduleRow of nextWeekScheduleRows.data ?? []) {
-    const scheduleId = scheduleRow.scheduleId
-    delete scheduleRow.scheduleId // Remove scheduleId from object, destructing behavior
-    const usersOfScheduleRow = Object.values(scheduleRow).filter(
-      (user) => user.User.email && user.User.notification
-    )
-
-    emailRecipients.push(
-      ...usersOfScheduleRow.map((user) => ({ scheduleId, ...user }))
-    )
-  }
-
   
-if (debugMode == "true") {
-  const names = emailRecipients.map((recipient) => recipient.User.firstName)
-  await resend.emails.send({
-    from: "ME <business@lasseaakjaer.com>",
-    to: "lasse_aakjaer@hotmail.com",
-    subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
-    text: JSON.stringify(names) + " was sent email of schedule week",
-  })
-  
-  await rollSchedules()
+    }
 
-  return NextResponse.json({ query: emailRecipients }, { status: 200 })
-} 
+    const supabase = createServerComponentClient({ cookies })
+    const role = await supabase.from("Role").select("id").limit(1)
 
-  for (const recipient of emailRecipients) {
-    const responseResend = await resend.emails.send({
-      from: "ME <business@lasseaakjaer.com>",
-      to: [recipient.User.email],
-      subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
-      react: NotifyUsersOfSchedule({
-        username: `${recipient.User.firstName} ${recipient.User.lastName}`,
-        scheduleId: recipient.scheduleId,
-      }),
-    })
-
-    console.info(
-      "Email sent to ",
-      recipient.User.email,
-      " for roomId: ",
-      responseResend.data?.id
-    )
-  }
-  
-  await resend.emails.send({
-    from: "ME <business@lasseaakjaer.com>",
-    to: "lasse_aakjaer@hotmail.com",
-    subject: `Do your duty!, Cleaning week ${currentWeekNumber + 1}`,
-    text: JSON.stringify(emailRecipients) + " was sent email of schedule week",
-  })
-  
-
-
-  
-  return NextResponse.json({ query: emailRecipients }, { status: 200 })
+    return NextResponse.json({ status: "OK" }, { status: 200 })
 }
-
